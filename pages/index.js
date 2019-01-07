409fffb53c85f054a4d5d28483cdca84e0169769
Anthony Oyathelemhi <tonero91@gmail.com>
2019-01-07 18:11:00 +0100

    setup bootsrap/jquery and update react to 16.7.0-alpha.0 to use hooks

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,26 @@
-const Index = () => (
-  <div>
-    <p>Welcome</p>
-  </div>
-);
+import Page from "../layout/main";
+import { useState, useEffect } from "react";
+
+const Index = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    $(".text-danger").css("font-size", "2rem");
+  });
+
+  return (
+    <Page>
+      <div>
+        <p className="text-danger">You clicked {count} times</p>
+        <button
+          className="btn btn-lg btn-primary rounded-0"
+          onClick={() => setCount(count + 1)}
+        >
+          Click me
+        </button>
+      </div>
+    </Page>
+  );
+};
 
 export default Index;
