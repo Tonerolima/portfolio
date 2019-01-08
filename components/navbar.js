1918d21e2e23fdd546a4e1ded6316c4d96482ecd
Anthony Oyathelemhi <tonero91@gmail.com>
2019-01-08 04:57:56 +0100

    implement animated navbar on scroll

--- components/navbar.js
+++ components/navbar.js
@@ -1,61 +1,107 @@
-const Navbar = () => {
-  return (
-    <nav className="navbar navbar-light bg-transparent navbar-expand-lg fixed-top">
-      <div className="container">
-        <h2 className="navbar-brand">
-          ANTH<span>O</span>NY
-        </h2>
-        <div className="navbar-collapse collapse">
-          <ul className="navbar-nav">
-            <li className="nav-item active">
-              <a href="#" className="nav-link">
-                HOME
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="#" className="nav-link">
-                ABOUT ME
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="#" className="nav-link">
-                PROJECTS
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="#" className="nav-link">
-                TECH STACK
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="#" className="nav-link">
-                CONTACT ME
-              </a>
-            </li>
-          </ul>
+import { useEffect } from "react";
+import { Component } from "react";
+
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.calcScroll = this.calcScroll.bind(this);
+
+    this.state = {
+      scrolled: false
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.calcScroll);
+  }
+
+  calcScroll() {
+    const position = window.scrollY;
+    const scrolled = this.state.scrolled;
+
+    if (position < 100) {
+      $(".navbar")
+        .removeClass("visible")
+        .css("top", "0px");
+      this.setState({ scrolled: false });
+    } else if (position < 200 && !scrolled) {
+      $(".navbar").css("top", "-100px");
+      this.setState({ scrolled: false });
+    } else if (position > 200 && !scrolled) {
+      $(".navbar")
+        .addClass("visible")
+        .animate({ top: "0px" });
+      this.setState({ scrolled: true });
+    }
+  }
+
+  render() {
+    return (
+      <nav className="navbar navbar-light bg-transparent navbar-expand-lg">
+        <div className="container">
+          <h2 className="navbar-brand">
+            ANTH<span>O</span>NY
+          </h2>
+          <div className="navbar-collapse collapse">
+            <ul className="navbar-nav">
+              <li className="nav-item active">
+                <a href="#" className="nav-link">
+                  HOME
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#" className="nav-link">
+                  ABOUT ME
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#" className="nav-link">
+                  PROJECTS
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#" className="nav-link">
+                  TECH STACK
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#" className="nav-link">
+                  CONTACT ME
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
 
-      <style jsx>{`
-        .navbar-brand {
-          margin-right: 350px;
-          font-size: 2rem;
-        }
+        <style jsx>{`
+          .navbar-brand {
+            margin-right: 350px;
+            font-size: 2rem;
+          }
 
-        .nav-link {
-          font-size: 0.9rem;
-        }
+          .navbar.visible {
+            position: fixed;
+            width: 100vw;
+            z-index: 11;
+            background: #fff !important;
+            box-shadow: 0px 1px 5px 1px #f5f4f4;
+          }
 
-        span {
-          color: #f9899c;
-        }
+          .nav-link {
+            font-size: 0.9rem;
+          }
 
-        * {
-          font-family: poppins;
-        }
-      `}</style>
-    </nav>
-  );
-};
+          span {
+            color: #f9899c;
+          }
+
+          * {
+            font-family: poppins;
+          }
+        `}</style>
+      </nav>
+    );
+  }
+}
 
 export default Navbar;
