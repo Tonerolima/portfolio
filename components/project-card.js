28fb05047275be4d4f35dd78eac9ed3ea8b39e82
Anthony Oyathelemhi <tonero91@gmail.com>
2019-01-12 20:42:15 +0100

    add project links to respective cards

--- components/project-card.js
+++ components/project-card.js
@@ -1,12 +1,14 @@
-const ProjectCard = ({ name, desc, stack, image }) => {
+const ProjectCard = ({ name, desc, stack, image, url }) => {
   return (
     <div className="card bg-dark text-white">
-      <img className="card-img" src={image} alt={name} />
-      <div className="card-img-overlay d-flex flex-column">
-        <h5 className="card-title mt-auto">{name}</h5>
-        <p className="card-text">{desc}</p>
-        <small className="card-text">Powered by: {stack}</small>
-      </div>
+      <a href={url} target="_blank">
+        <img className="card-img" src={image} alt={name} />
+        <div className="card-img-overlay d-flex flex-column">
+          <h5 className="card-title mt-auto">{name}</h5>
+          <p className="card-text">{desc}</p>
+          <small className="card-text">Powered by: {stack}</small>
+        </div>
+      </a>
       <style jsx>
         {`
           img {
@@ -19,6 +21,10 @@ const ProjectCard = ({ name, desc, stack, image }) => {
           }
           .card:hover {
             transform: scale(0.9);
+          }
+
+          a {
+            all: unset;
           }
         `}
       </style>
--- components/projects.js
+++ components/projects.js
@@ -6,51 +6,58 @@ const Projects = () => {
       name: "Shifter",
       desc: "A JAM stack web app for a fictional courier service company",
       stack: "Node, Express, Mongodb, React, Next, Bootstrap 4, jQuery",
-      image: "/static/projects/shifter.png"
+      image: "/static/projects/shifter.png",
+      url: "http://shifter.cf/"
     },
     {
       name: "CYON - Regina Mundi",
       desc:
         "A server-side rendered website for the catholic youth organization, Mushin Lagos",
       stack: "Node, Express, Mongodb, Semantic-UI, jQuery, Bootstrap",
-      image: "/static/projects/cyon.png"
+      image: "/static/projects/cyon.png",
+      url: "https://cyon-regina-mundi.herokuapp.com"
     },
     {
       name: "Fast-Food-Fast",
       desc: "A JAM stack web app for a fictional food delivery company",
       stack: "Node, Express, PostgreSQL, React",
-      image: "/static/projects/fast-food-fast.png"
+      image: "/static/projects/fast-food-fast.png",
+      url: "http://fff-react.herokuapp.com"
     },
     {
       name: "YelpCamp",
       desc: "Website for finding camp sites to share and visit",
       stack: "Node, Express, Mongodb, Semantic-UI",
-      image: "/static/projects/yelp-camp.png"
+      image: "/static/projects/yelp-camp.png",
+      url: "https://damp-mountain-50073.herokuapp.com"
     },
     {
       name: "Pixel Art Maker",
       desc:
         "A single page website for creating digital art with an adjustable canvas to draw on",
       stack: "HTML, CSS, JavaScript",
-      image: "/static/projects/pixel-art-maker.png"
+      image: "/static/projects/pixel-art-maker.png",
+      url: "https://tonerolima.github.io/Pixel-Art-Maker/"
     },
     {
       name: "RGB Color Game",
       desc:
         "A simple game where users try to select the correct color in the displayed RGB representation",
       stack: "HTML, CSS, JavaScript",
-      image: "/static/projects/color-game.png"
+      image: "/static/projects/color-game.png",
+      url: "http://tonerolima.github.io/ColorGame"
     }
   ];
 
   const renderList = () => {
-    return projectList.map(({ name, desc, stack, image }, index) => {
+    return projectList.map(({ name, desc, stack, image, url }, index) => {
       return (
         <ProjectCard
           name={name}
           desc={desc}
           stack={stack}
           image={image}
+          url={url}
           key={index}
         />
       );
